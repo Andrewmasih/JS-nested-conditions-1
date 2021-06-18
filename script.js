9bb7ad9dbdc738a2e56d51d045fe17e587bc0bc4
Andrew Masih <armasih@outlook.com>
2021-06-18 01:01:42 +0000

    added more comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* 'nested condition' is a conditional statement nested inside another conditional statement, it check if the condiontion is TRUE or FALSE, bu creating a new 'if/else' block inside of an exisisting 'if/else' blocks*/
 
 if (2 > 1) {
-  console.log("if, statement1 is excuted");
+  console.log("if condition is excuted");
   if (1 > 0) {
-    console.log("nested if condition1 is excuted");
+    console.log("nested 'if' condition1 is excuted");
   }
 }
 
